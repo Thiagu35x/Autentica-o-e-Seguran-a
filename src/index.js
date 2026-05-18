605ff8b3aa547da4a0bb644506ed7abebfc8c290
Thiago Castro <thiagocastro799@gmail.com>
2026-05-18 02:27:24 +0000

    fix: remove app.listen do src/index.js para Vercel

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
-
 import models, { sequelize } from './models/index.js';
 import { authMiddleware, protectRoutes } from './middlewares/auth.js';
 import sessionRoutes from './routes/session.js';
@@ -9,28 +8,15 @@ import userRoutes from './routes/user.js';
 import messageRoutes from './routes/message.js';
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use((req, res, next) => {
-  req.context = { models };
-  next();
-});
-
+app.use((req, res, next) => { req.context = { models }; next(); });
 app.use(authMiddleware);
 app.use(protectRoutes);
-
 app.use('/session', sessionRoutes);
 app.use('/users', userRoutes);
 app.use('/messages', messageRoutes);
 
-// Deixamos o sync normal sem o "force" para a Vercel não travar o banco
-sequelize.sync({}).then(() => {
-  app.listen(process.env.PORT || 3000, () =>
-    console.log(`Servidor ativo`),
-  );
-}).catch(err => console.log('Erro de banco:', err));
-
+export { sequelize };
 export default app;
